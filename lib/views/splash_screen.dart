5351fd0b79207ac4b5a393a08e17ff3d94248003
victor <dvicxy@gmail.com>
2021-09-08 13:55:13 +0100

    new commit

--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -13,7 +13,7 @@ class _SplashState extends State<Splash> {
   @override
   void initState() {
     Timer(
-        Duration(seconds: 1),
+        Duration(seconds: 5),
         () => Navigator.pushReplacement(
             context, MaterialPageRoute(builder: (context) => LandingScreen())));
     super.initState();
